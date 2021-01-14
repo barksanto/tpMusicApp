4944dd04ddf9e2bee105dd700287d263b17cbc5f
Barkley Santo <barkley.santo@gmail.com>
2021-01-14 16:38:04 -0600

    see list of top 10 albums done

--- public/index.js
+++ public/index.js
@@ -11,54 +11,77 @@ document.addEventListener("DOMContentLoaded", event => {
   const meatLoaf = db.collection('myAlbums').doc('Bat Out of Hell');
   const pinkFloyd = db.collection('myAlbums').doc('The Dark Side of the Moon');
   const whitney = db.collection('myAlbums').doc('The Bodyguard');
+  const beegees = db.collection('myAlbums').doc('Saturday Night Fever');
+  const fleetwood = db.collection('myAlbums').doc('Rumours');
+  const shania = db.collection('myAlbums').doc('Come On Over');
 
+  shania.onSnapshot(doc => {
+    const shaniaData = doc.data();
+    shaniaData.songs.forEach((song) => {
+      document.querySelector('.tenthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 pt song-name ml-2">${song}</h3>`)
+    })
+  })
+
+  fleetwood.onSnapshot(doc => {
+    const fleetwoodData = doc.data();
+    fleetwoodData.songs.forEach((song) => {
+      document.querySelector('.ninthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
+    })
+  })
+
+  beegees.onSnapshot(doc => {
+    const beegeesData = doc.data();
+    beegeesData.songs.forEach((song) => {
+      document.querySelector('.eighthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
+    })
+  })
 
   whitney.onSnapshot(doc => {
     const whitneyData = doc.data();
     whitneyData.songs.forEach((song) => {
-      document.querySelector('.seventhAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`)
+      document.querySelector('.seventhAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
     })
   })
 
   pinkFloyd.onSnapshot(doc => {
     const pinkFloydData = doc.data();
     pinkFloydData.songs.forEach((song) => {
-      document.querySelector('.sixthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`)
+      document.querySelector('.sixthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
     })
   })
 
   meatLoaf.onSnapshot(doc => {
     const meatLoafData = doc.data();
     meatLoafData.songs.forEach((song) => {
-      document.querySelector('.fifthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`)
+      document.querySelector('.fifthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
     })
   })
 
   acdc.onSnapshot(doc => {
     const acdcData = doc.data();
     acdcData.songs.forEach((song) => {
-      document.querySelector('.fourthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`)
+      document.querySelector('.fourthAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
     })
   })
 
   michael.onSnapshot(doc => {
     const michaelData = doc.data();
     michaelData.songs.forEach((song) => {
-      document.querySelector('.thirdAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`)
+      document.querySelector('.thirdAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
     })
   })
 
   eagles.onSnapshot(doc => {
     const eaglesData = doc.data();
     eaglesData.songs.forEach((song) => {
-      document.querySelector('.secondAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`)
+      document.querySelector('.secondAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`)
     })
   })
 
   queen.onSnapshot(doc => {
     const qData = doc.data();
     qData.songs.forEach((song) => {
-      document.querySelector('.firstAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name">${song}</h3>`);
+      document.querySelector('.firstAlbum').insertAdjacentHTML('beforeend', `<h3 class="px-2 song-name ml-2">${song}</h3>`);
     })
   })
 
