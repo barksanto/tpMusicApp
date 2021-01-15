17a37a3ec065f32c6056cd778ea2856764de7320
Barkley Santo <barkley.santo@gmail.com>
2021-01-15 16:25:28 -0600

    media added with functioning audio and pay button

--- public/albums.js
+++ public/albums.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", event => {
         `<div class="container pt-3">
           <div class="row">
             <li class="px-2 song-name new-card mx-1 col-6">${song}</li>
-            <i class="far fa-play-circle col-2"></i>
+            <i class="far fa-play-circle col-2 play-btn"></i>
           </div>
          </div>`);
     })
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", event => {
         <div class="container pt-3">
           <div class="row">
             <li class="px-2 song-name new-card mx-1 col-6">${song}</li>
-            <i class="far fa-play-circle col-2"></i>
+            <i class="far fa-play-circle col-2 play-btn"></i>
             </div>
          </div>`);
     })
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", event => {
         <div class="container pt-3">
           <div class="row">
            <li class="px-2 song-name new-card mx-1 col-6">${song}</li>
-           <i class="far fa-play-circle col-2"></i>
+           <i class="far fa-play-circle col-2 play-btn"></i>
            </div>
          </div>`);
     })
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", event => {
         <div class="container pt-3">
           <div class="row">
             <li class="px-2 song-name new-card mx-1 col-6">${song}</li>
-            <i class="far fa-play-circle col-2"></i>
+            <i class="far fa-play-circle col-2 play-btn"></i>
             </div>
          </div>`);
     })
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", event => {
           <div class="container pt-3">
            <div class="row">
             <li class="px-2 song-name new-card mx-1 col-6">${song}</li>
-             <i class="far fa-play-circle col-2"></i>
+             <i class="far fa-play-circle col-2 play-btn"></i>
              </div>
              </div>`);
     })
@@ -95,4 +95,83 @@ document.addEventListener("DOMContentLoaded", event => {
 
 
 
+})
+
+// get the elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const playerVideo = player.querySelector('.player__video');
+
+// play media code
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value
+  // console.log(this.value);
+  // console.log(this.name);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  // console.log(e)
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function changeSize() {
+  console.log('clicked fullscreen');
+  playerVideo.style.width = `100vw`;
+  // playerVideo.style.height = `100vh`;
+}
+
+// Hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', () => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+fullScreen.addEventListener('click', changeSize);
+
+const playBtns = document.querySelector('.play-btn');
+console.log(playBtns)
+playBtns.forEach((btn) => {
+  btn.addEventListener('click', togglePlay)
 })
